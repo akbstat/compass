fefc7d09f192c3926c65995f5b5529343c79d76b
yuki <yukichen@gmx.com>
2025-08-20 12:21:24 +0800

    add app env in configuration

--- bin/compass/server/main.rs
+++ bin/compass/server/main.rs
@@ -1,7 +1,7 @@
 use compass::{init_router, Config};
 use std::error::Error;
 use tokio::net::TcpListener;
-use tracing::{info, Level};
+use tracing::info;
 use tracing_appender::{non_blocking, rolling};
 
 #[tokio::main]
@@ -9,7 +9,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let config = Config::new()?;
     let (logger, _guard) = non_blocking(rolling::daily(&config.log_dir, "app.log"));
     tracing_subscriber::fmt()
-        .with_max_level(Level::DEBUG)
+        .with_max_level(config.log_level)
         .with_ansi(false)
         .with_writer(logger)
         .json()
--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,18 @@
 use crate::errors::Result;
 use dotenv::dotenv;
-use std::env;
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 use tracing::Level;
 
 #[derive(Debug)]
 pub struct Config {
-    pub log_dir: String,
+    pub log_dir: PathBuf,
     pub database_url_vestige: String,
     pub database_url_sdtm: String,
     pub addr: String,
+    pub env: String,
     pub log_level: Level,
 }
 
@@ -18,22 +22,26 @@ impl Config {
         let addr = format!("0.0.0.0:{}", env::var("SERVER_PORT")?);
         let database_url_vestige = env::var("DATABASE_URL_VESTIGE")?;
         let database_url_sdtm = env::var("DATABASE_URL_SDTM")?;
-        let log_dir = env::var("LOG_DIR")?;
-        let log_level = match env::var("LOG_LEVEL") {
+
+        let (log_level, env) = match env::var("COMPASS_ENV") {
             Ok(value) => {
-                if value.eq("DEV") {
-                    Level::DEBUG
-                } else {
+                let log_level = if value.eq("PROD") {
                     Level::INFO
-                }
+                } else {
+                    Level::DEBUG
+                };
+                (log_level, value.to_lowercase())
             }
-            Err(_) => Level::INFO,
+            Err(_) => (Level::INFO, "prod".to_string()),
         };
+
+        let log_dir = Path::new(&env::var("LOG_DIR")?).to_path_buf();
         Ok(Config {
             log_dir,
             database_url_vestige,
             database_url_sdtm,
             addr,
+            env,
             log_level,
         })
     }
